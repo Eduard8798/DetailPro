4104e5f5f620ad4480ad92d19bc5f0241692fb2d
Eduard8798 <eduard9999997@gmail.com>
2025-04-03 15:52:18 +0300

    init commit

---
+++ my-cleaning-app/models/Booking.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const BookingSchema = new mongoose.Schema({
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    workType: { type: String, required: true }, // Полная/частичная чистка
+    carType: { type: String, required: true }, // Тип кузова
+    interiorMaterial: { type: String, required: true }, // Материал салона
+}, { timestamps: true });
+
+export default mongoose.models.Booking || mongoose.model("Booking", BookingSchema);
---
+++ my-cleaning-app/src/app/page.js
@@ -0,0 +1,12 @@
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <a href={'/'}>General</a>
+            <a href={'/about'}>About</a>
+        </div>
+    );
+};
+
+export default Page;
